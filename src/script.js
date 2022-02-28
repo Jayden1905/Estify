2b3ed2e173966fca0ba3a5d8547f1d479d4a024d
Jayden1905 <yawzyn001@mymail.sim.edu.sg>
2022-02-28 22:25:32 +0630

    add tab menu

--- src/script.js
+++ src/script.js
@@ -1,6 +1,9 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const menuItems = document.querySelector(".menu-items");
 const menuLinks = document.querySelectorAll(".menu-link");
+const tabs = document.querySelectorAll("[data-tab-target]");
+const search = document.querySelector(".search");
+const searchText = document.querySelector(".search-text");
 
 menuToggle.addEventListener("click", () => {
   menuItems.classList.toggle("hidden");
@@ -11,3 +14,7 @@ menuLinks.forEach((n) =>
     menuItems.classList.add("hidden");
   })
 );
+
+search.addEventListener("click", () => {
+  searchText.classList.toggle("hidden");
+});
