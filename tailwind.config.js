6557c442f3a00c6abc437878ed4745efd5d67331
Jayden1905 <yawzyn001@mymail.sim.edu.sg>
2022-03-03 01:05:16 +0630

    fix tab bars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
+      },
+      gridTemplateRows: {
+        "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
+      },
       colors: {
         TextPrimary: "#F8F9FA",
         TextSecondary: "black",
