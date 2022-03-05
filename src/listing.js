efc1a201a414797d8d2dafceaae7faf28c0ffdbf
Jayden1905 <yawzyn001@mymail.sim.edu.sg>
2022-03-06 00:39:29 +0630

    add imgae slider

--- src/listing.js
+++ src/listing.js
@@ -4,6 +4,13 @@ const menuItems = document.getElementById("menu-items");
 const menuLinks = document.querySelectorAll(".menu-link");
 const navBarList = document.querySelector(".listing-nav-bar");
 const ulList = document.querySelector(".ul-list");
+const btnHeart = document.querySelector(".btn-heart");
+const heart = document.querySelector(".heart");
+
+btnHeart.addEventListener("click", () => {
+  //Not working and don't know why
+  heart.classList.toggle("heart-active");
+});
 
 $(document).ready(function () {
   $(window).click(function (e) {
@@ -26,11 +33,31 @@ $(document).ready(function () {
     if ($(this).scrollTop() > yourHeader) {
       $(".listing-nav-bar").addClass("fixed");
       $(".listing-nav-bar").addClass("shadow-xl");
-      $(".nav-list").css("height", "6rem");
+      $(".nav-list").css("height", "5rem");
+      $(".product-listing").addClass("top-20");
     } else {
       $(".listing-nav-bar").removeClass("fixed");
       $(".listing-nav-bar").removeClass("shadow-xl");
       $(".nav-list").css("height", "3rem");
+      $(".product-listing").removeClass("top-20");
     }
+  });
+});
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]");
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
   });
 });
